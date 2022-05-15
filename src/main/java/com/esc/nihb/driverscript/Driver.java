24dfff521e1bfaddd823d5a192d0543d49ff47bf
sudharanimsc <sudharanimsc@gmail.com>
2022-05-15 18:34:59 -0400

    nihb project

---
+++ src/main/java/com/esc/nihb/driverscript/Driver.java
@@ -0,0 +1,12 @@
+package com.esc.nihb.driverscript;
+
+import com.esc.nihb.utility.framework.ExecuteTestScript;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		  ExecuteTestScript executeTestScript = new ExecuteTestScript();
+		  executeTestScript.run();
+		 	}
+}
